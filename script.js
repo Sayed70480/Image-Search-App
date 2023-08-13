a18e5edc449b40d89f397045c9834da04585d778
Syed sikandar Shah <88287750+Sayed70480@users.noreply.github.com>
2023-08-13 16:47:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const accessKey = "gRCQj383IDYMX4kcp5XAiOCiZlWtkzNaQ1sh2dn2G3I";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results; // Corrected property name
+    if (page === 1) {
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink); // Corrected variable name
+        searchResults.appendChild(imageWrapper);
+    });
+
+    page++;
+    if (page > 1) {
+        showMore.style.display = "block";
+    }
+}
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () => {
+    searchImages();
+});
